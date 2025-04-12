06e01b951310f93a12189f19c79ba6255a1a423b
dineshpandeyy <dineshpandey3536@gmail.com>
2025-04-12 09:50:23 -0500

    different error handling while fetching

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+// import StarRating from './StarRating';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    {/* <StarRating /> */}
   </React.StrictMode>
 );
